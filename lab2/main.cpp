454ad36b1b8edd8a1f7645fbd011987bc32302cb
Natalia <natalia_maltseva@bk.ru>
2019-09-27 21:13:27 +0300

    размытие работает, нужно проверить соответствие blur

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -12,23 +12,42 @@ int main() {
     Mat myimg=imread("dog.jpg", IMREAD_GRAYSCALE); // convert image to the single channel grayscale image
     int vertice_x, vertice_y = 0;
     Mat roi;
-    Mat data;
-    //blur(myimg, data, Size(3,3));
-    for (vertice_y = 0; vertice_y < myimg.rows-3; vertice_y+=3) {
-        for (vertice_x = 0; vertice_x < myimg.cols-3; vertice_x+=3) {
+    double sum ;
+    //Mat filter = myimg.clone();
+    //blur(filter, filter, Size(3,3));
+    imwrite("Saves/non_filtered_dog.jpg", myimg);
+    for (vertice_x = 0; vertice_x < myimg.cols-3; vertice_x+=3) {
+        for (vertice_y = 0; vertice_y < myimg.rows-3; vertice_y+=3) {
+            sum = 0;
             // Select ROI
             Rect RectangleToSelect(vertice_x, vertice_y, 3, 3);
             roi = myimg(RectangleToSelect);
-            data = roi.clone(); //roi is non-continuous Mat, copy it to reshape and count average
-            int roi_size = data.rows * data.cols;
+            //imshow("Filter", roi);
+
+            //waitKey(0);
+            //data = roi.clone(); //roi is non-continuous Mat, copy it to reshape and count average
+            /*int roi_size = data.rows * data.cols;
             data = data.reshape(1, roi_size); //reshape 1 channel matrix of size rows x cols to be a matrix of shape ((rows*cols) x 1)
             reduce(data, data, 1, REDUCE_AVG); // compute the average row-wise
-            data = data.reshape(1, roi_size); ////reshape to 1 channel matrix
+            data = data.reshape(1, roi_size); //reshape to 1 channel matrix*/
+            for (int row=0; row<3; row++) {
+                for (int col = 0; col<3; col++ ) {
+                    sum += roi.at<uchar>(row,col);
+                }
+            }
+            for (int row=0; row<3; row++) {
+                for (int col = 0; col<3; col++ ) {
+                    myimg.at<uchar>(vertice_y+col, vertice_x+row) = (int)sum/9;
+                }
+            }
         }
-        cout<<"End loop 1"<<endl;
+        //if (vertice_x == 960)
+          //  cout<<"vertice_x =  "<< vertice_x<< "vertice_y = " << vertice_y << endl;
     }
-    cout<<"End loop 2"<<endl;
-    imshow("Filter", data);
+    //cout<<"End loop 2"<<endl;
+    imwrite("Saves/filtered_dog.jpg", myimg);
+    imshow("Filter", myimg);
+
 
     waitKey(0);
 
